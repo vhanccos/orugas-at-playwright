e2e95fd3471056e203a51904ffd812e498fa1657
joeCuadros <jcuadrosam@unsa.edu.pe>
2025-07-04 16:38:48 -0500

    feat: Ejemplo de test a validar

--- tests/Ejemplo_titulo.spec.ts
+++ tests/Ejemplo_titulo.spec.ts
@@ -1,8 +1,33 @@
 import { test, expect } from '@playwright/test';
+import { isAuthValid } from '../scripts/checkAuth.js';
+import path from 'path';
 
+const storagePath = path.resolve('auth/storageState.json');
+const urlBase = 'https://teammates-orugas.appspot.com/';
+
+// validar el login inicial
+test.beforeAll(() => {
+  	if (! isAuthValid(storagePath)) {
+    	throw new Error(
+      	'❌ La sesion de Google ha expirado o no existe. ' +
+      	'Ejecuta `node scripts/saveAuth.js` para regenerar auth/storageState.json'
+    	);
+  	}
+});
+
+// configurar contexto
+test.use({
+  	storageState: storagePath
+});
+
+// Empezar los tests
 test('El titulo de TEAMMATES', async ({ page }) => {
-  await page.goto('https://teammates-orugas.appspot.com/'); //url inicial
+	await page.goto(urlBase + 'web/instructor/home'); //url inicial
+	// Esperar carga 
+	await page.waitForLoadState('load');
+    await page.waitForLoadState('domcontentloaded');
+    await page.waitForLoadState('networkidle');
 
-  await expect(page).toHaveTitle(/TEAMMATES/); //si el titulo contiene TEAMMATES
-  
+	await expect(page.locator('h1')).toContainText('Home');
+	await page.screenshot({ path: 'screenshots/prueba.png' });
 });
